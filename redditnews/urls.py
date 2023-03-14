881e5cc1f252d64c00e7afd4ddf5cc492222469d
HumaIlyas <hi.wtmconsult@gmail.com>
2023-03-14 15:06:37 +0000

    Add installed apps in settings.py and url in base.html

--- redditnews/urls.py
+++ redditnews/urls.py
@@ -11,7 +11,7 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('news/', include('news.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("news.urls"), name="news-urls"),
     path('summernote/', include('django_summernote.urls')),
+    path("accounts/", include("allauth.urls")),
 ]
