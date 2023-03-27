85f7abdf766024a9a0f3202b9f12972d6b9542f4
HumaIlyas <hi.wtmconsult@gmail.com>
2023-03-27 19:31:21 +0000

    update test_models

--- news/test_models.py
+++ news/test_models.py
@@ -11,7 +11,7 @@ class TestCategory(TestCase):
         mocked = datetime.datetime(2018, 4, 4, 0, 0, 0, tzinfo=pytz.utc)
         with mock.patch('django.utils.timezone.now', mock.Mock(return_value=mocked)):
             category = Category.objects.create(title='test', category='test')
-            self.assertTrue(category.created_on, mocked)
+            self.assertEqual(category.created_on(), mocked)
 
 
 class TestPost(TestCase):
@@ -63,13 +63,13 @@ class TestPost(TestCase):
             mocked = datetime.datetime(2018, 4, 4, 0, 0, 0, tzinfo=pytz.utc)
             with mock.patch('django.utils.timezone.now', mock.Mock(return_value=mocked)):
                 post = Post.objects.create(title='test', post='test')
-                self.assertEqual(post.created_on, mocked)
+                self.assertEqual(post.created_on(), mocked)
 
         def test_updated_on_auto_now_True(self):
             mocked = datetime.datetime(2018, 4, 4, 0, 0, 0, tzinfo=pytz.utc)
             with mock.patch('django.utils.timezone.now', mock.Mock(return_value=mocked)):
                 post = Post.objects.create(title='test', post='test')
-                self.assertEqual(post.updated_on, mocked)
+                self.assertEqual(post.updated_on(), mocked)
 
         def test_ordering_are_explicit_in_post_metaclass(self):
             ordering = post._meta.ordering
@@ -121,7 +121,7 @@ class TestComment(TestCase):
             mocked = datetime.datetime(2018, 4, 4, 0, 0, 0, tzinfo=pytz.utc)
             with mock.patch('django.utils.timezone.now', mock.Mock(return_value=mocked)):
                 comment = Comment.objects.create(body='test', name='test')
-                self.assertEqual(comment.created_on, mocked)
+                self.assertEqual(comment.created_on(), mocked)
 
         def test_approved_defaults_to_false(self):
             comment = Comment.objects.create(comment='Test news Comment')
