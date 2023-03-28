1469aa1e8838b63f3f3d6472c77f6adaf462870b
HumaIlyas <hi.wtmconsult@gmail.com>
2023-03-28 15:23:26 +0000

    Update files

--- news/urls.py
+++ news/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.PostList.as_view(), name='home'),
-    path("", views.CommentList.as_view(), name='post_detail'),
+    path("", views.CommentList.as_view(), name='news/post_detail'),
     path('category/<str:category>', views.PostCategory.as_view(), name='category'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='news/post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='news/post_like'),
 ]
