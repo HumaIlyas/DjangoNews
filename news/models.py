2b5397c1ca9747cf3e51c9b1a498ea6d2ae429ad
HumaIlyas <hi.wtmconsult@gmail.com>
2023-04-02 07:34:19 +0000

    Update models.py

--- news/models.py
+++ news/models.py
@@ -19,15 +19,14 @@ class Post(models.Model):
     category = models.ForeignKey(Category, null=True, on_delete=models.CASCADE)
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name="posts")
+    author = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name="news_posts")
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(
-        User, related_name='post_like', blank=True)
+    likes = models.ManyToManyField(User, related_name='news_likes', blank=True)
 
     class Meta:
         ordering = ["-created_on"]
@@ -40,8 +39,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE,
-                             related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     name = models.CharField(max_length=80)
     email = models.EmailField()
     comment = models.TextField()
