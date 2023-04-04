02473567ea03ded7d2986cc24cc035a27b63e674
HumaIlyas <>
2023-04-04 18:27:11 +0000

    Update setting.py

--- redditnews/settings.py
+++ redditnews/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["ci-fsf-django-news.herokuapp.com", "localhost", "8000-humailyas-djangonews-2gjqhnflse.us2.codeanyapp.com"]
 
