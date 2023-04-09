2ab5c9bd048116c7fc8514fbc74ff42d81793c0e
HumaIlyas <>
2023-04-09 21:58:14 +0000

    Add alret messages for sign in, sign out & registation pages

--- news/views.py
+++ news/views.py
@@ -19,7 +19,6 @@ class PostCategory(View):
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
-    permission_required = ("add_post", "delete_post")
     template_name = "news/index.html"
     paginate_by = 6
 
