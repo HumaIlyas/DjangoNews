dd8043080e9d8f2d9fd5256c98f08286b4e2ecd6
HumaIlyas <hi.wtmconsult@gmail.com>
2023-04-11 20:22:58 +0000

    Update comment count

--- news/urls.py
+++ news/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.CommentList.as_view(), name='news/post_detail'),
     path('category/<str:category>', views.PostCategory.as_view(), name='category'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='news/post_detail'),
+    path('<slug:slug>/', views.PostComment.as_view(), name='home'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='like_post'),
     path('delete/<slug:slug>', views.PostDelete.as_view(), name='delete_post'),
     path('approve/<slug:slug>', views.CommentApproval.as_view(), name='approve_comment'),
