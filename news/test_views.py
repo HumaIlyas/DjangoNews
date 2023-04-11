4f5696f6b4d44df03a92905929ded5342225d2f9
HumaIlyas <hi.wtmconsult@gmail.com>
2023-04-11 18:28:32 +0000

    Add test views

--- news/test_views.py
+++ news/test_views.py
@@ -72,6 +72,25 @@ class TestPostLike(TestCase):
         self.assertEqual(post.likes.first(), self.user)
         self.assertFalse('news/post_detail.html' in test_response.context)
 
+class TestPostDelete(TestCase):
+    def setUp(self):
+        """
+        Setup for testing
+        """
+        self.user = User.objects.create_user(
+            username='test user',
+            email='test@email.com',
+            password='testpass',
+        )
+        self.user.save()
+    def test_can_delete_post(self):
+            post = Post.objects.create(title='Test post')
+            test_response = self.client.get('/posts/')
+            self.assertEqual(test_response.status_code, 404)
+            self.assertEqual(post.delete(), (1, {'news.Post': 1}))
+            self.assertFalse('news/post_detail.html' in test_response.context)
+            
+
 class TestCommentList(TestCase):    
     def test_comment_list(self):
         test_response = self.client.get('/comments/')
