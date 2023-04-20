540ce51f2cf066f81644800532c5eefa920166a4
HumaIlyas <hi.wtmconsult@gmail.com>
2023-04-20 16:40:08 +0000

    Update message.test.js

--- static/scripts/message.js
+++ static/scripts/message.js
@@ -1,10 +1,7 @@
-import * as bootstrap from 'bootstrap';
-window.bootstrap = bootstrap;
-
 setTimeout(function () {
             let messages = document.getElementById("msg");
             let alert = new bootstrap.Alert(messages);
             alert.close();
 }, 3000);
 
-module.exports = {setTimeout, bootstrap};
+module.exports = {setTimeout};
--- static/scripts/tests/message.test.js
+++ static/scripts/tests/message.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const {setTimeout} = require("../message", "bootstrap");
+const {setTimeout} = require("../message");
 
 it('should set alert to "close" after 3000 ms', (done) => {
     setTimeout(() => {
