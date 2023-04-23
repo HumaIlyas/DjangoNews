58af67bed94eb57fe1f4d6a6b36cf0954e6d104a
HumaIlyas <hi.wtmconsult@gmail.com>
2023-04-23 14:50:06 +0000

    install whitenoise

--- redditnews/settings.py
+++ redditnews/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["ci-fsf-django-news.herokuapp.com", "localhost"]
 
@@ -103,16 +103,16 @@ WSGI_APPLICATION = 'redditnews.wsgi.application'
 #Database
 #https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 #DATABASES = {
-#   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#    'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#      'NAME': BASE_DIR / 'db.sqlite3',
+#   }
 #}
+
+DATABASES = {
+   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
